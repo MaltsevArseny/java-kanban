db6dcaaac8adf550cbf185d21835bad693c085a0
MaltsevArseny <arsenymaltsev2005@gmail.com>
2025-11-03 09:26:03 +0200

    Delete src directory

--- src/managers/ManagerSaveException.java
+++
@@ -1,7 +0,0 @@
-package managers;
-
-public class ManagerSaveException extends RuntimeException {
-    public ManagerSaveException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
